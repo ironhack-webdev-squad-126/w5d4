29bc4b90af9c0e125a5a9cfdf9c5f56480c20302
mjarraya <mjarraya@student.42.fr>
2019-04-18 16:28:21 +0200

    ajax put with axios

--- ajax-advanced/index.js
+++ ajax-advanced/index.js
@@ -2,15 +2,27 @@ const baseURL = 'http://localhost:1337';
 
 document.getElementById('update-form').onsubmit = event => {
   event.preventDefault();
+
   const name = document.getElementById('name-update').value;
   const occupation = document.getElementById('occupation-update').value;
   const location = document.getElementById('location-update').value;
   const id = document.getElementById('id-update').value;
+
   axios
     .put(`${baseURL}/superheroes/${id}`, { name, occupation, location })
     .then(response => {
       const { data } = response;
-      console.log(data);
+      const { id, name, occupation, location } = data;
+
+      const updatedCharacterHTML = `
+        <p>${id}</p>
+        <p>${name}</p>
+        <p>${occupation}</p>
+        <p>${location}</p>
+      `;
+
+      document.getElementById(id).innerHTML = updatedCharacterHTML;
+      document.getElementById('update-form').reset();
     })
     .catch(err => {
       console.error(err);
@@ -19,15 +31,17 @@ document.getElementById('update-form').onsubmit = event => {
 
 document.getElementById('character-form').onsubmit = event => {
   event.preventDefault();
+
   const name = document.getElementById('name-input').value;
   const occupation = document.getElementById('occupation-input').value;
   const location = document.getElementById('location-input').value;
+
   axios
     .post(`${baseURL}/superheroes`, { name, occupation, location })
     .then(response => {
       const { data } = response;
-      console.log(data);
       const { id, name, occupation, location } = data;
+
       const newCharacterHTML = `
       <li id=${id}>
           <p>${id}</p>
@@ -36,6 +50,7 @@ document.getElementById('character-form').onsubmit = event => {
           <p>${location}</p>
       </li>
       `;
+
       document.getElementById('characters-list').innerHTML += newCharacterHTML;
       document.getElementById('character-form').reset();
     })
@@ -47,8 +62,10 @@ document.getElementById('character-form').onsubmit = event => {
 const getCharacters = () => {
   axios.get(`${baseURL}/superheroes`).then(response => {
     const { data } = response;
+
     data.forEach(character => {
       const { id, name, occupation, location } = character;
+
       const newCharacterHTML = `
         <li id=${id}>
             <p>${id}</p>
@@ -57,6 +74,7 @@ const getCharacters = () => {
             <p>${location}</p>
         </li>
         `;
+
       document.getElementById('characters-list').innerHTML += newCharacterHTML;
     });
   });
